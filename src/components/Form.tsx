cad8ef6e252f08ae657547d596de985d29e96005
klazbaba <giwaklaz@gmail.com>
2020-08-26 16:52:02 +0100

    added keyboardVerticalOffset prop

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,12 +3,16 @@ import { KeyboardAvoidingView, Platform } from 'react-native';
 
 interface Props {
   children: Element | Element[];
+  keyboardVerticalOffset?: number;
 }
 
 export default function Form(props: Props) {
   return (
     <KeyboardAvoidingView
-      keyboardVerticalOffset={Platform.OS === 'ios' ? 50 : undefined}
+      keyboardVerticalOffset={
+        Platform.OS == 'ios' ? props.keyboardVerticalOffset || 50 : undefined
+      }
+      behavior={Platform.OS == 'ios' ? 'padding' : undefined}
     >
       {props.children}
     </KeyboardAvoidingView>
