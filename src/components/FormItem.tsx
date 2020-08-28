233e6ebd2ed4d33bbfbd30fc0b5ae9ad4aaea9b6
klazbaba <giwaklaz@gmail.com>
2020-08-28 18:34:38 +0100

    updated FormItem

--- src/components/FormItem.tsx
+++ src/components/FormItem.tsx
@@ -1,74 +1,76 @@
-// import React, { forwardRef } from 'react';
-// import { TextInput, TextInputProperties, View, StyleSheet } from 'react-native';
+import React, { forwardRef } from 'react';
+import {
+  TextInput,
+  TextInputProperties,
+  View,
+  StyleSheet,
+  Text,
+} from 'react-native';
 
-// interface Props extends TextInputProperties {
-//   textInputStyle?: object;
-//   children?: Element;
-//   hasError?: boolean;
-//   underneathText?: string;
-//   underneathTextStyle?: object;
-//   label?: string;
-//   labelStyle?: object;
-//   isRequired?: boolean;
-// }
+import Label from '../components/Label';
 
-// const FormItem = forwardRef(({ children, ...props }: Props, ref) => {
-//   return (
-//     <>
-//       {props.label && (
-//         <Label
-//           title={props.label}
-//           style={props.labelStyle}
-//           isRequired={props.isRequired}
-//         />
-//       )}
-//       <View style={[styles.wrapper, props.style]}>
-//         {
-//           // this is separated from props because adding it causes TextInput to throw an exception
-//           children
-//         }
-//         <TextInput
-//           {...props}
-//           style={[styles.inputText, props.textInputStyle]}
-//           // @ts-ignore
-//           ref={ref}
-//         />
-//         {props.hasError && (
-//           <Icon
-//             name="error"
-//             type="MaterialIcons"
-//             style={{ color: colors.primary }}
-//           />
-//         )}
-//       </View>
-//       {!!props.underneathText && (
-//         <ShrText
-//           title={props.underneathText}
-//           style={[styles.underneathText, props.underneathTextStyle]}
-//         />
-//       )}
-//     </>
-//   );
-// });
+interface Props extends TextInputProperties {
+  textInputStyle?: object;
+  children?: Element;
+  hasError?: boolean;
+  underneathText?: string;
+  underneathTextStyle?: object;
+  text?: string;
+  labelStyle?: object;
+  isRequired?: boolean;
+}
 
-// const styles = StyleSheet.create({
-//   wrapper: {
-//     flexDirection: 'row',
-//     backgroundColor: colors.white,
-//     borderRadius: 5,
-//     paddingHorizontal: 8,
-//     minHeight: 48,
-//     alignItems: 'center',
-//   },
-//   inputText: {
-//     flex: 1,
-//     paddingVertical: 8,
-//   },
-//   underneathText: {
-//     marginTop: 8,
-//     marginLeft: 8,
-//     color: colors.white,
-//   },
-// });
+const FormItem = forwardRef(({ children, ...props }: Props, ref) => {
+  return (
+    <>
+      {props.text && (
+        <Label
+          text={props.text}
+          style={props.labelStyle}
+          isRequired={props.isRequired}
+        />
+      )}
+      <View style={[styles.wrapper, props.style]}>
+        {
+          // this is separated from props because adding it causes TextInput to throw an exception
+          children
+        }
+        <TextInput
+          {...props}
+          style={[styles.inputText, props.textInputStyle]}
+          // @ts-ignore
+          ref={ref}
+        />
+        {props.hasError && <Text>{'\u026A0'}</Text>}
+      </View>
+      {!!props.underneathText && (
+        <Label
+          text={props.underneathText}
+          style={[styles.underneathText, props.underneathTextStyle]}
+        />
+      )}
+    </>
+  );
+});
 
-// export default FormItem;
+const styles = StyleSheet.create({
+  wrapper: {
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderRadius: 5,
+    paddingHorizontal: 8,
+    minHeight: 48,
+    alignItems: 'center',
+  },
+  inputText: {
+    flex: 1,
+    paddingVertical: 8,
+  },
+  underneathText: {
+    marginTop: 8,
+    marginLeft: 8,
+    color: 'white',
+  },
+});
+
+export default FormItem;
