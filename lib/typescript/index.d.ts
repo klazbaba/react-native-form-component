00ec55f839e4f318f79cf6604f211e1882db3159
klazbaba <giwaklaz@gmail.com>
2020-09-02 11:48:03 +0100

    updated typings

--- lib/typescript/index.d.ts
+++ lib/typescript/index.d.ts
@@ -1,7 +1,3 @@
 import Form from './components/Form';
 import Label from './components/Label';
-declare const _default: {
-    Form: typeof Form;
-    Label: typeof Label;
-};
-export default _default;
+export { Form, Label };
