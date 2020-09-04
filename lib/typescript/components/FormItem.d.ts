e5c3cb910183813145a0c372d9def228c542b139
klazbaba <giwaklaz@gmail.com>
2020-09-04 13:40:07 +0100

    updated typings

--- lib/typescript/components/FormItem.d.ts
+++ lib/typescript/components/FormItem.d.ts
@@ -9,7 +9,6 @@ interface Props extends TextInputProperties {
     labelStyle?: object;
     isRequired?: boolean;
     value: string;
-    hasError: boolean;
 }
 declare const FormItem: React.ForwardRefExoticComponent<Props & React.RefAttributes<unknown>>;
 export declare const isErrorFree: (keyboardType: "default" | "email-address" | "numeric" | "phone-pad" | "number-pad" | "decimal-pad" | "visible-password" | "ascii-capable" | "numbers-and-punctuation" | "url" | "name-phone-pad" | "twitter" | "web-search" | undefined, isRequired: boolean, value: string) => boolean;
