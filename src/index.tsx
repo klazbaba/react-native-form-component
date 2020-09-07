26509277ef8b311fbeae677865d50f22267ba4aa
klazbaba <giwaklaz@gmail.com>
2020-09-07 02:56:36 +0100

    now exporting FormItemProps

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import Form from './components/Form';
 import Label from './components/Label';
 import FormItem from './components/FormItem';
+import FormItemProps from './components/FormItem';
 
-export { Form, Label, FormItem };
+export { Form, Label, FormItem, FormItemProps };
