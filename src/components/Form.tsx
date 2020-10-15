37e3cf1b2514f81e94013e6aadc8218b3b04b097
klazbaba <giwaklaz@gmail.com>
2020-10-15 19:54:20 +0100

    added submitForm to indx.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,6 +5,8 @@ import {
   Pressable,
   Text,
   StyleSheet,
+  TextInputSubmitEditingEventData,
+  NativeSyntheticEvent,
 } from 'react-native';
 
 import { containsError } from './FormItem';
@@ -19,7 +21,10 @@ interface Props {
   onButtonPress: () => void;
 }
 
-export let submitForm: Function;
+export let submitForm: (
+  e: NativeSyntheticEvent<TextInputSubmitEditingEventData>
+) => void;
+
 export default function Form(props: Props) {
   const [width, setWidth] = useState(0);
   const [opacity, setOpacity] = useState(1);
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import Form from './components/Form';
 import Label from './components/Label';
 import FormItem from './components/FormItem';
+import { submitForm } from './components/Form';
 
-export { Form, Label, FormItem };
+export { Form, Label, FormItem, submitForm };
