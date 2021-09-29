1cde43b0fcf34b67d7b3be833431d7c1dbcb77cf
klazbaba <giwaklaz@gmail.com>
2021-09-29 20:03:29 +0100

    ran bob

--- lib/typescript/components/Label.d.ts
+++ lib/typescript/components/Label.d.ts
@@ -2,6 +2,7 @@ interface Props {
     text: string;
     asterik?: boolean;
     style?: object | object[];
+    textStyle?: object | object[];
 }
 export default function Label(props: Props): JSX.Element;
 export {};
