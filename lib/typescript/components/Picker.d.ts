093ba3ccd17aeff77f3bd2e0f8adeca347c475cb
klazbaba <giwaklaz@gmail.com>
2021-10-05 15:50:20 +0100

    ran bob

--- lib/typescript/components/Picker.d.ts
+++ lib/typescript/components/Picker.d.ts
@@ -8,14 +8,15 @@ interface Props {
     onSelection: (item: Item) => void;
     selectedValue: ReactText;
     pickerIcon?: ReactNode;
+    iconWrapperStyle?: object | object[];
     asterik?: boolean;
     labelStyle?: object | object[];
     asterikStyle?: object | object[];
     label?: ReactText;
+    labelWrapperStyle?: object | object[];
     placeholder?: string;
     selectedValueStyle?: object | object[];
     buttonStyle?: object | object[];
-    childWrapperStyle?: object | object[];
     itemLabelStyle?: object | object[];
 }
 export default function Picker(props: Props): JSX.Element;
