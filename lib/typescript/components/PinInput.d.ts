46e3211ab081aa9629affb57b85cd7408b1ca047
klaz <giwaklaz@gmail.com>
2022-11-02 13:58:24 +0100

    ran bob

---
+++ lib/typescript/components/PinInput.d.ts
@@ -0,0 +1,6 @@
+interface Props {
+    numOfInput: number;
+    onChangeText: (pin: string) => string;
+}
+export default function PinInput(props: Props): JSX.Element;
+export {};
--- lib/typescript/index.d.ts
+++ lib/typescript/index.d.ts
@@ -4,4 +4,5 @@ import FormItem from './components/FormItem';
 import { submitForm } from './components/Form';
 import Picker from './components/Picker';
 import Modal from './components/Modal';
-export { Form, Label, FormItem, submitForm, Picker, Modal };
+import PinInput from './components/PinInput';
+export { Form, Label, FormItem, submitForm, Picker, Modal, PinInput };
