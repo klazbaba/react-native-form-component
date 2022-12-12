dcea2c58066d2ccaa996ad9788cd8d972fd68fa3
klaz <giwaklaz@gmail.com>
2022-12-12 13:40:41 +0100

    styling updated

--- lib/typescript/components/PinInput.d.ts
+++ lib/typescript/components/PinInput.d.ts
@@ -1,9 +1,10 @@
 /// <reference types="react" />
-import { TextStyle } from 'react-native';
+import { TextStyle, ViewStyle, StyleProp } from 'react-native';
 interface Props {
     numOfInput: number;
     onChangeText: (pin: string) => void;
     textInputStyle?: TextStyle;
+    style?: StyleProp<ViewStyle>;
 }
 export default function PinInput(props: Props): JSX.Element;
 export {};
